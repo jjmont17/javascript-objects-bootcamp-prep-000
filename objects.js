062944ac3d1e2a1fbed5deaa1d2c1243bb93bd4b
jmontenegro@uh.edu <jmontenegro@uh.edu>
2018-05-30 16:07:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,13 @@
-playlist
+var playlist = { 'artist' : 'songs'};
+
+function updatePlaylist(playlist, artistName, songName)
+{
+  playlist[artustName] = songName
+  return playlist;
+}
+
+function removeFromPlaylist( playlist, artistName)
+{
+  delete playlist[artistName]
+  return playlist;
+}
